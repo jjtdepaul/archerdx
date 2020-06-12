d8ab365e6ccfc2991920d5b7116692a9296b2888
james depaul <jjtdepaul@yahoo.com>
2020-06-12 16:52:36 -0600

    Added :latest to docker build

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Archer - from this simple NodeJS app');
+  res.send('Hello from Archer app!');
 });
 
 var server = app.listen(3000, function () {
