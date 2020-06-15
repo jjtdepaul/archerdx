58a721e0f8fa7172c6cef466fea35a5df81e2090
james depaul <jjtdepaul@yahoo.com>
2020-06-15 10:07:51 -0600

    Version 2.0 of the app

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello from Archer app!');
+  res.send('Hello V2 from Archer app!');
 });
 
 var server = app.listen(3000, function () {
